ee539edcdf1bbd66d94e0fe711e8ccfc7fa53e79
viniciusquintana <137747453+viniciusquintana@users.noreply.github.com>
2024-02-15 03:18:55 -0300

    Finished the 12th lesson

---
+++ Lesson-12/javascript/index.js
@@ -0,0 +1,15 @@
+//let age = 21;
+//let message = age >= 18 ? "You're an adult" : "You're a minor";
+//console.log (message);
+
+//let time = 9;
+//let greeting = time < 12 ? "Good morning!" : "Good afternoon!";
+//console.log(greeting);
+
+//let isStudent = true;
+//let message = isStudent ? "You are a student" : "You are NOT a student";
+//console.log(message);
+
+let purchaseAmount = 125;
+let discount = purchaseAmount >=100 ? 10 : 0;
+console.log (`Your total is $${purchaseAmount - purchaseAmount * (discount/100)}`);
