24b619a91bc8188f04a1328b4a2b26ba8d7cf178
viniciusquintana <137747453+viniciusquintana@users.noreply.github.com>
2024-02-14 21:44:02 -0300

    finished the 2nd lesson

---
+++ Lesson-2/javascript/index.js
@@ -0,0 +1,7 @@
+let fullName = "Vinícius Quintana"
+let age = 22;
+let isStudent = false;
+
+document.getElementById("p1").textContent = `Your name is ${fullName}`;
+document.getElementById("p2").textContent = `You are ${age} years old`;
+document.getElementById("p3").textContent = `Enrolled: ${isStudent}`;
