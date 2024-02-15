e4dbb3f3668b656f7368626ec524de32b75c153f
viniciusquintana <137747453+viniciusquintana@users.noreply.github.com>
2024-02-15 04:02:22 -0300

    finished the 14th lesson

---
+++ Lesson-14/javascript/index.js
@@ -0,0 +1,17 @@
+//let userName = "Vinícius";
+
+//let result = userName.includes(" ");
+
+//if(result){
+//  console.log("Your username can't inclue ' '")
+//}
+
+//else{
+//  console.log(userName);
+//}
+
+let phoneNumber = "123-456-7890";
+
+phoneNumber = phoneNumber.padEnd(15, "0");
+
+console.log(phoneNumber);
