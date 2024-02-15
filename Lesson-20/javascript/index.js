1b2a224ae67d0fd540eadf8495c6776efdef9c10
viniciusquintana <137747453+viniciusquintana@users.noreply.github.com>
2024-02-15 16:21:45 -0300

    finished the 20th lesson

---
+++ Lesson-20/javascript/index.js
@@ -0,0 +1,15 @@
+//for(let i = 10; i > 0; i-=1){
+//  console.log(i);
+//}
+
+//console.log("HAPPY NEW YEAR!");
+
+for(let i = 1; i <= 20; i++){
+
+  if(i == 13){
+    break;
+  }
+  else {
+    console.log(i);
+  }
+}
