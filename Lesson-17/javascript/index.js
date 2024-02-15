0cc558c91692252be58b82ce3b9a5bb23a2b7bbe
viniciusquintana <137747453+viniciusquintana@users.noreply.github.com>
2024-02-15 15:10:36 -0300

    finished the 17th lesson

---
+++ Lesson-17/javascript/index.js
@@ -0,0 +1,21 @@
+//const temp = 10;
+
+//if(temp > 0 && temp <= 30) {
+//  console.log("The weather is GOOD");
+//}
+
+//if(temp <= 0 || temp > 30) {
+//  console.log("The weather is BAD");
+//}
+//else{
+//  console.log("The weather is GOOD");
+//}
+
+const isSunny = false;
+
+if(!isSunny){
+  console.log("It is CLOUDY");
+}
+else{
+  console.log("It is SUNNY")
+}
