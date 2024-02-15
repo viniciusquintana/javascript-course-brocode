ad23c95a26aa26cc6383d5789b5587cf4648e80c
viniciusquintana <137747453+viniciusquintana@users.noreply.github.com>
2024-02-14 21:14:05 -0300

    finished the 1st lesson

---
+++ Lesson-1/javascript/index.js
@@ -0,0 +1,16 @@
+//console.log ('hello');
+//console.log ('i like pizza!')
+
+//window.alert('This is an alert')
+//window.alert('i like pizza!')
+
+//This is a comment
+document.getElementById("myH1").textContent = 'Hello';
+document.getElementById("myP").textContent = 'i like pizza!'
+
+/* 
+  This
+  is
+  a
+  comment
+*/
