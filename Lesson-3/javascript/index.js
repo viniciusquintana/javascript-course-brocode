971351be24c093278eb81928d1e5b0a51e28e0c3
viniciusquintana <137747453+viniciusquintana@users.noreply.github.com>
2024-02-14 22:06:43 -0300

    finished the 3rd lesson

---
+++ Lesson-3/javascript/index.js
@@ -0,0 +1,22 @@
+let students = 30;
+
+//students = students + 1;
+//students = students - 2;
+//students = students * 2;
+//students = students / 2;
+//students = students ** 2;
+//let extraStudents = students % 3;
+
+//students += 1;
+//students -=1;
+//students *=2;
+//students /= 2;
+//students **=2;
+//students %=2;
+
+//students++;
+//students--;
+
+let result = 6 / 2 ** (2+5);
+
+console.log(result);
