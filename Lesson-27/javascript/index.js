71ca63645478e9aa57a87b18cfe2b4ab1b4e0cb0
viniciusquintana <137747453+viniciusquintana@users.noreply.github.com>
2024-02-16 01:57:31 -0300

    finished the 27th lesson

---
+++ Lesson-27/javascript/index.js
@@ -0,0 +1,49 @@
+//function openFridge(...foods){
+  //console.log(...foods);
+//}
+//function getFood(...foods){
+  //return foods;
+//}
+
+//const food1 = "pizza";
+//const food2 = "hamburger";
+//const food3 = "hotdog";
+//const food4 = "sushi";
+//const food5 = "ramen";
+
+//openFridge(food1, food2, food3, food4, food5);
+
+//const foods = getFood(food1, food2, food3, food4, food5);
+
+//console.log(foods);
+
+//function sum(...numbers){
+//  let result = 0;
+//  for(let number of numbers){
+//    result += number;
+//  }
+//  return result;
+//}
+
+//const total = sum(1, 2, 3, 4, 5);
+
+//console.log(`Your total is $${total}`)
+
+//function getAverage(...numbers){
+//  let result = 0;
+//  for(let number of numbers){
+//    result += number;
+//  }
+//  return result / numbers.length;
+//}
+
+//const total = getAverage(75, 100, 85, 90, 50);
+
+//console.log(total);
+function combineStrings(...strings){
+  return strings.join(" ");
+}
+
+const fullName = combineStrings("Mr.", "Spongebob", "Squarepants", "III");
+
+console.log(fullName);
