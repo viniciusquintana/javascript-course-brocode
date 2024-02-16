ff96bd33d669c460ba2b7ea421f5feea7b94459d
viniciusquintana <137747453+viniciusquintana@users.noreply.github.com>
2024-02-16 01:38:00 -0300

    finished the 26th lesson

---
+++ Lesson-26/javascript/index.js
@@ -0,0 +1,17 @@
+//let numbers = [1, 2, 3, 4, 5];
+//let maximum= Math.max(...numbers);
+//let minimum = Math.min(...numbers);
+//console.log(maximum);
+//console.log(minimum);
+
+//let username = "Vinícius Quintana";
+//let letters = [...username].join("-");
+
+//console.log(letters);
+
+let fruits = ["apple", "orange", "banana"];
+let vegetables = ["carrots", "celery", "potatoes"];
+
+let foods = [...fruits, ...vegetables, "eggs", "milk"];
+
+console.log(foods);
