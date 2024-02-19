f02812a2f3831356294c3e2066449acbaf7ba57c
viniciusquintana <137747453+viniciusquintana@users.noreply.github.com>
2024-02-19 01:23:43 -0300

    finished the 56th lesson

---
+++ Lesson-56/javascript/index.js
@@ -0,0 +1,12 @@
+function func1(callback){
+  setTimeout(() => {console.log("Task 1"); callback()}, 3000);
+}
+
+function func2(){
+  console.log("Task 2");
+  console.log("Task 3");
+  console.log("Task 4");
+}
+
+func1(func2);
+
