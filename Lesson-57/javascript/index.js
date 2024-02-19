40ac321eafe45c08ff87d10158f3f9e0da535139
viniciusquintana <137747453+viniciusquintana@users.noreply.github.com>
2024-02-19 01:59:39 -0300

    finished the 57th lesson

---
+++ Lesson-57/javascript/index.js
@@ -0,0 +1,36 @@
+//try {
+//  console.log(x);
+  // NETWORK ERRORS
+  // PROMISE REJECTION
+  // SECURITY ERRORS
+//}
+//catch(error){
+//  console.error(error);
+//}
+//finally{
+  // CLOSE FILES
+  // CLOSE CONNECTIONS
+  // RELEASE RESOURCES
+//  console.log("This always executes")
+//}
+//console.log("You have reached the end!");
+
+try{
+  const divident = Number (window.prompt("Enter a divident: "));
+  const divisor = Number (window.prompt("Enter a divisor: "));
+
+  if(divisor == 0){
+    throw new Error("You can't divide by zero!");
+  }
+  if(isNaN(divident) || isNaN(divisor)){
+    throw new Error("Values must be a number");
+  }
+  
+  const result = divident  / divisor;
+  console.log(result);
+}
+catch(error){
+  console.error(error);
+}
+
+console.log("You have reached the end!");
