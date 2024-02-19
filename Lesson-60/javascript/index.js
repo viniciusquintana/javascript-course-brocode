c5fc5c3a8c3ed579aef664338749345b6306895b
viniciusquintana <137747453+viniciusquintana@users.noreply.github.com>
2024-02-19 03:55:26 -0300

    finished the 60th lesson

---
+++ Lesson-60/javascript/index.js
@@ -0,0 +1,38 @@
+//const myHeading = document.getElementById("my-heading");
+//myHeading.style.backgroundColor = "yellow";
+//myHeading.style.textAlign = "center";
+
+//console.log(myHeading);
+
+
+
+//const fruits =  document.getElementsByClassName("fruits");
+
+//Array.from(fruits).forEach(fruit => {
+//  fruit.style.backgroundColor = "yellow";
+//});
+
+//const h4Elements = document.getElementsByTagName("h4");
+//const liElements = document.getElementsByTagName("li");
+
+//Array.from(h4Elements).forEach(h4Element => {
+//  h4Element.style.backgroundColor = "yellow"
+//});
+
+//Array.from(liElements).forEach(liElement => {
+//  liElement.style.backgroundColor = "lightgreen"
+//});
+
+
+
+//const element = document.querySelector("ul");
+
+//element.style.backgroundColor = "yellow";
+
+
+
+const foods = document.querySelectorAll("li");
+
+foods.forEach(food => {
+  food.style.backgroundColor = "yellow";
+})
