1007c87e59fa8ff18d2265fe7aa2d975db38706e
viniciusquintana <137747453+viniciusquintana@users.noreply.github.com>
2024-02-19 16:43:00 -0300

    finished the 61st lesson

---
+++ Lesson-61/javascript/index.js
@@ -0,0 +1,48 @@
+//const element = document.getElementById("desserts");
+//const firstChild = element.firstElementChild;
+//firstChild.style.backgroundColor = "yellow";
+
+//const ulElements = document.querySelectorAll("ul");
+
+//ulElements.forEach(ulElement => {
+//  const firstChild = ulElement.firstElementChild;
+//  firstChild.style.backgroundColor = "yellow";
+//})
+
+//-------------------------------------------------------
+
+//const element = document.getElementById("desserts");
+//const lastChild = element.lastElementChild;
+//lastChild.style.backgroundColor = "yellow";
+
+//const ulElements = document.querySelectorAll("ul");
+
+//ulElements.forEach(ulElement => {
+//  const lastChild = ulElement.lastElementChild;
+//  lastChild.style.backgroundColor = "yellow";
+//})
+
+//---------------------------------------------------------
+
+//const element = document.getElementById("vegetables");
+//const nextSibling = element.nextElementSibling;
+//nextSibling.style.backgroundColor = "yellow";
+
+//---------------------------------------------------------
+
+//const element = document.getElementById("desserts");
+//const prevSibling = element.previousElementSibling;
+//prevSibling.style.backgroundColor = "yellow";
+
+//---------------------------------------------------------
+
+//const element = document.getElementById("pie");
+//const parent = element.parentElement;
+//parent.style.backgroundColor = "yellow";
+
+//---------------------------------------------------------
+
+const element = document.getElementById("vegetables");
+const children = element.children;
+
+children[1].style.backgroundColor = "yellow";
