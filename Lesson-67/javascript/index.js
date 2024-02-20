c191a6edd1321723d7efc5333da838f031c78f50
viniciusquintana <137747453+viniciusquintana@users.noreply.github.com>
2024-02-20 06:17:11 -0300

    finished the 67th lesson

---
+++ Lesson-67/javascript/index.js
@@ -0,0 +1,75 @@
+// const myH1 = document.getElementById("myH1");
+// const myButton = document.getElementById("myButton");
+
+// myButton.classList.add("enabled");
+// myButton.classList.remove("enabled");
+
+
+
+// myButton.addEventListener("mouseover", event =>{
+//   event.target.classList.toggle("hover");
+// });
+
+// myButton.addEventListener("mouseout", event =>{
+//   event.target.classList.toggle("hover");
+// });
+
+
+
+// myH1.classList.add("enabled");
+// myH1.addEventListener("click", event => {
+
+//   if(event.target.classList.contains("disabled")){
+//     event.target.textContent += "🤬";
+//   }
+//   else{
+//     event.target.classList.replace("enabled", "disabled");
+
+//   }
+// });
+
+// myButton.classList.add("enabled");
+// myButton.addEventListener("click", event => {
+
+//   if(event.target.classList.contains("disabled")){
+//     event.target.textContent += "🤬";
+//   }
+//   else{
+//     event.target.classList.replace("enabled", "disabled");
+
+//   }
+// });
+
+
+
+let buttons = document.querySelectorAll(".myButtons");
+
+buttons.forEach(button => {
+  button.classList.add("enabled");
+});
+
+buttons.forEach(button => {
+  button.addEventListener("mouseover", event => {
+    event.target.classList.toggle("hover");
+  });
+});
+
+buttons.forEach(button => {
+  button.addEventListener("mouseout", event => {
+    event.target.classList.toggle("hover");
+  });
+});
+
+buttons.forEach(button => {
+  button.addEventListener("click", event => {
+
+    if(event.target.classList.contains("disabled")){
+      event.target.textContent += "🤬";
+    }
+    else {
+      event.target.classList.replace("enabled", "disabled");
+    }
+
+    event.target.classList.replace("enabled", "disabled");
+  });
+});
